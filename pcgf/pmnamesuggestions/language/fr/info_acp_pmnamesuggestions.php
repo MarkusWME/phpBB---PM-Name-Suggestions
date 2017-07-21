bcfc75fb66c3c4543f5737a5202e16d3ee759b6a
Raphaël M <Galixte@users.noreply.github.com>
2017-07-21 17:29:29 +0200

    Update of the FRENCH translation for the PM Name Suggestions extension

--- pcgf/pmnamesuggestions/language/fr/info_acp_pmnamesuggestions.php
+++ pcgf/pmnamesuggestions/language/fr/info_acp_pmnamesuggestions.php
@@ -1,32 +1,50 @@
 <?php
-
 /**
- * @author    MarkusWME <markuswme@pcgamingfreaks.at>
- * @copyright 2016, 2017 MarkusWME
- * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
- * @version   1.1.0
  *
- * French translation by Kenjiraw (https://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=1688226)
+ * PM Name Suggestions. An extension for the phpBB Forum Software package.
+ * French translation by Kenjiraw (https://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=1688226) & Galixte (http://www.galixte.com)
+ *
+ * @copyright (c) 2016, 2017 MarkusWME <markuswme@pcgamingfreaks.at>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
  */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
-    exit;
+	exit;
 }
 
 if (empty($lang) || !is_array($lang))
 {
-    $lang = array();
+	$lang = array();
 }
 
-// Merging language data for the ACP with the other language data
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-    'ACP_PCGF_PMNAMESUGGESTIONS'                           => 'MP Suggestions de Noms',
-    'ACP_PCGF_PMNAMESUGGESTIONS_AVATAR_IMAGE_SIZE'         => 'Taille de l\'Avatar',
-    'ACP_PCGF_PMNAMESUGGESTIONS_AVATAR_IMAGE_SIZE_EXPLAIN' => 'Définis la largeur et la hauteur de l\'avatar à la valeur donnée.<br/>Pour cacher l\'avatar vous pouvez définir cette valeur à 0.',
+    'ACP_PCGF_PMNAMESUGGESTIONS'                           => 'Noms suggérés dans les MP',
+    'ACP_PCGF_PMNAMESUGGESTIONS_AVATAR_IMAGE_SIZE'         => 'Dimensions des avatars',
+    'ACP_PCGF_PMNAMESUGGESTIONS_AVATAR_IMAGE_SIZE_EXPLAIN' => 'Permet de définir la largeur et la hauteur des avatars affichés dans la liste des suggestions.<br/>Définir sur 0 pour masquer l’affichage des avatars.',
     'ACP_PCGF_PMNAMESUGGESTIONS_SETTINGS'                  => 'Paramètres',
-    'ACP_PCGF_PMNAMESUGGESTIONS_SETTINGS_EXPLAIN'          => 'Ici vous pouvez ajuster les paramètres pour MP Suggestions de Noms.',
-    'ACP_PCGF_PMNAMESUGGESTIONS_SETTINGS_SAVED'            => 'Les paramètres ont été sauvegardés avec succès !',
+    'ACP_PCGF_PMNAMESUGGESTIONS_SETTINGS_EXPLAIN'          => 'Sur cette page il est possible de configurer les paramètres de l’extension « PM Name Suggestions ».',
+    'ACP_PCGF_PMNAMESUGGESTIONS_SETTINGS_SAVED'            => 'Les paramètres ont été sauvegardés avec succès !',
     'ACP_PCGF_PMNAMESUGGESTIONS_USER_COUNT'                => 'Nombre de suggestions',
-    'ACP_PCGF_PMNAMESUGGESTIONS_USER_COUNT_EXPLAIN'        => 'Le nombre maximum d\'utilisateurs proposés (valeurs comprises entre 0 et 100).',
+    'ACP_PCGF_PMNAMESUGGESTIONS_USER_COUNT_EXPLAIN'        => 'Permet de définir le nombre maximum de noms d’utilisateurs suggérés (valeurs acceptées : entre 0 et 100).',
 ));
